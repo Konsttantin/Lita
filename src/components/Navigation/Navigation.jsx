53d378e5e9b8b4e14388794e8f0fd812dfe872eb
Konstantin <ivanchenkomypost@gmail.com>
2023-09-02 22:27:28 +0300

    fixed svg paths

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,13 +5,13 @@ const Navigation = () => {
   return (
     <ul className={cl.navigation}>
       <li className={cn(cl.item, { [cl.active]: false })}>
-        <img src="./src/assets/SVG/menu.svg" alt="Menu" />
+        <img src="public/SVG/menu.svg" alt="Menu" />
       </li>
       <li className={cn(cl.item, { [cl.active]: false })}>
-        <img src="./src/assets/SVG/home.svg" alt="Home" />
+        <img src="public/SVG/home.svg" alt="Home" />
       </li>
       <li className={cn(cl.item, { [cl.active]: true })}>
-        <img src="./src/assets/SVG/calendar.svg" alt="Calendar" />
+        <img src="public/SVG/calendar.svg" alt="Calendar" />
       </li>
     </ul>
   );
